a9d8fb8d00cb556b65aad3ba57e022e56891d6cd
Yuji Tateno <webmaster@tarosuke.net>
2024-12-31 01:24:53 +0900

    著作権表示の年のtypoを修正

--- sources/canvas.cc
+++ sources/canvas.cc
@@ -1,6 +1,6 @@
 // /**********************************************************************
 // Canvas
-//  * Copyright (C) 2020, 2924 tarosuke<webmaster@tarosuke.net>
+//  * Copyright (C) 2020, 2024 tarosuke<webmaster@tarosuke.net>
 //  *
 //  * This program is free software; you can redistribute it and/or
 //  * modify it under the terms of the GNU General Public License
